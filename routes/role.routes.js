4040d2f6d3ae3a59761ff1e4ea5f24e4dd085d5d
Aryan576 <aryanshah576@gmail.com>
2022-12-03 22:17:32 +0530

    Roles delete routes updated done

--- routes/role.routes.js
+++ routes/role.routes.js
@@ -7,5 +7,6 @@ router.post('/addRole',controller.addRole)
 router.get('/getRoles',controller.getRoles)
 router.get('/getRolesByID/:roleid',controller.getRolesByID)
 router.put('/updateRole/:roleid',controller.updateRole)
+router.delete('/deleteRole/:roleid',controller.deleteRole)
 
 module.exports=router
